26db85b47d8ea7520d56191d9d2aa4e946a51f2c
zacharyrb99 <zach.boudreaux999@gmail.com>
2021-11-28 14:43:26 -0500

    finished README.md

--- src/API.js
+++ src/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5000";
-const BASE_URL = "https://capstone2-foodly-backend.herokuapp.com/";
+const BASE_URL = "https://capstone2-foodly-backend.herokuapp.com";
 
 class API {
     // Token used for authorization
